5b8f3ca176a4a513076fd2050dc326ee0df0662a
WraithRR8 <cauaelore@gmail.com>
2026-08-23 15:11:21 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
+meme_dict = {
+            "CRINGE": "Algo vergonhoso ou constrangedor",
+            "STALKEAR": "Investigar a vida de alguém online",
+            "GANKAR": "Oferecer ajuda para outra lane passando fome",
+            "FREEZAR": "Segurar os minions próximo a minha torre",
+            "GIVAR": "Deixar um objetivo de lado",
+            "AFK": "Abandonar o time, por que precisou sair",
+            }
 
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Não há essa palavra no dicionário!")
